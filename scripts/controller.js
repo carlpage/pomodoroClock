5c01e0e263fc8aed1fbef099e0c4b65009563ca7
Carl Page <crlpage7@gmail.com>
2017-09-04 10:52:56 -0500

    yup yup

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,29 +1,4 @@
-function smallestCommons(arr) {
-  var num1 = arr[0];
-  var num2 = arr[1];
-  var newArr1 = [num1];
-  var newArr2 = [num2];
-  var bool = true;
-  var nextNum1 = num1;
-  var nextNum2 = num2;
-  var lcm;
 
-  while (bool === true) {
-    nextNum1 = nextNum1 + num1;
-    nextNum2 = nextNum2 + num2;
-    newArr1.push(nextNum1);
-    newArr2.push(nextNum2);
-    console.log(nextNum2);
-    for (var i = 0; i < newArr1.length; i++) {
-      if(newArr1.indexOf(newArr2[i]) !== -1) {
-        lcm = newArr1[i];
-        bool = false;
-      }
-    } // end for loop
-  } // end while loop
-  console.log(lcm);
-  return lcm;
-} // end function
 
 smallestCommons([1,5]);
 
